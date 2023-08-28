ad22de580fd92cd3170639509a2cd57c47aa8295
breejohnson <53101509+breejohnson@users.noreply.github.com>
2023-08-28 14:03:32 -0700

    Add files via upload
    Simple program to ask for user input (Zodiac sign) and then give user out put (Traits of said sign).

---
+++ Astrological Sign Trait Generator.py
@@ -0,0 +1,25 @@
+#Astrological trait generator
+#input  
+Signs = input("Your sign:  ")
+if Signs == "Aquarius":
+    print("You are considered cerebral, quirky and creative.")
+elif Signs == "Pisces":
+    print("You are considered Moody, kindhearted and emotional.")
+elif Signs == "Aries": 
+    print("You are considered Energetic, Brave and Snappy.")
+elif Signs == "Taurus":
+    print("You are considered Caring, stubborn, and hardworking.")
+elif Signs == "Gemini":
+    print("You are considered Funny, confusing, and sociable.")
+elif Signs == "Cancer":
+    print("You are considered Lovable, playful and saracastic.")
+elif Signs == "Leo":
+    print("You are considered Fun, egotistical, and outgoing. ")
+elif Signs == "Virgo":
+    print("You are considered Intelligent, Loyal, and Compromising.")
+elif Signs == "Libra":
+    print("You are considered Ambiverted, flighty and friendly.")
+elif Signs == "Saggitarius":
+    print("You are considered Hilarious, generous and loyal.")
+elif Signs == "Capricorn":
+    print("You are considered Kind, Suspicious and ambitious.")
